8360dbca8fbfa957fa9dd3dbaf6e60b3df723f41
Rohith <tangadipallirohith@gmail.com>
2023-11-19 14:15:39 -0500

    Added curves and clear function

--- app/src/main/java/com/rohith/mapping/MainActivity.java
+++ app/src/main/java/com/rohith/mapping/MainActivity.java
@@ -1,11 +1,13 @@
 package com.rohith.mapping;
 
-import android.graphics.Color;
+import android.Manifest;
+import android.content.Context;
 import android.content.pm.PackageManager;
+import android.graphics.Color;
 import android.location.Location;
+import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
@@ -14,6 +16,7 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.FragmentActivity;
+
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapView;
@@ -21,11 +24,13 @@ import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.PolylineOptions;
 
-public class MainActivity extends FragmentActivity implements OnMapReadyCallback {
+public class MainActivity extends FragmentActivity implements OnMapReadyCallback, LocationListener {
     private MapView mapView;
     private GoogleMap googleMap;
     private PolylineOptions polylineOptions;
     private static final int LOCATION_PERMISSION_REQUEST_CODE = 1;
+    private LocationManager locationManager;
+    private boolean isTracking = false;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -36,75 +41,39 @@ public class MainActivity extends FragmentActivity implements OnMapReadyCallback
         mapView.onCreate(savedInstanceState);
         mapView.getMapAsync(this);
 
-        // Request location permissions
-        requestLocationPermissions();
+        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
 
-        // Initialize PolylineOptions
-        polylineOptions = new PolylineOptions();
-
-        // Start Tracking Button Click
         Button startTrackingButton = findViewById(R.id.startTrackingButton);
         startTrackingButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                // Clear previous polyline if any
-                polylineOptions = new PolylineOptions();
-                polylineOptions.width(5);  // Set the width of the polyline
-                polylineOptions.color(Color.BLUE);  // Set the color of the polyline
-
-                // Add the current location to the new polyline
-                addCurrentLocationToPolyline();
+                isTracking = true;
+                requestLocationUpdates();
             }
         });
 
-        // Stop Tracking Button Click
+        Button clearButton = findViewById(R.id.clearButton);
+        clearButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                clearAllLines();
+            }
+        });
+
         Button stopTrackingButton = findViewById(R.id.stopTrackingButton);
         stopTrackingButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
-                addCurrentLocationToPolyline();
-
-                // Draw the current polyline on the map
-                if (googleMap != null) {
-                    googleMap.addPolyline(polylineOptions);
-                }
+                isTracking = false;
+                locationManager.removeUpdates(MainActivity.this);
             }
         });
     }
 
-    private void requestLocationPermissions() {
-        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
-                && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            ActivityCompat.requestPermissions(this,
-                    new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION, android.Manifest.permission.ACCESS_COARSE_LOCATION},
-                    LOCATION_PERMISSION_REQUEST_CODE);
-        }
-    }
-
-    @Override
-    public void onMapReady(GoogleMap googleMap) {
-        this.googleMap = googleMap;
-
-        // Check if location permissions are granted
-        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED
-                && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
-            // Location permissions are granted, enable location tracking
-            googleMap.setMyLocationEnabled(true);
-
-            // Get the user's current location
-            LocationManager locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
-            Location location = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
-
-            if (location != null) {
-                // Get the latitude and longitude of the user's current location
-                double latitude = location.getLatitude();
-                double longitude = location.getLongitude();
-
-                // Move the camera to the user's current location
-                LatLng currentLocation = new LatLng(latitude, longitude);
-                googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(currentLocation, 15));
-            }
+    private void clearAllLines() {
+        if (googleMap != null && polylineOptions != null) {
+            googleMap.clear();
+            polylineOptions = null;
         }
     }
 
@@ -112,24 +81,64 @@ public class MainActivity extends FragmentActivity implements OnMapReadyCallback
     protected void onResume() {
         super.onResume();
         mapView.onResume();
+        if (isTracking) {
+            requestLocationUpdates();
+        }
     }
 
     @Override
     protected void onPause() {
         super.onPause();
         mapView.onPause();
+        locationManager.removeUpdates(this);
+    }
+
+    private void requestLocationUpdates() {
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED
+                && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 1000, 10, this);
+        } else {
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION},
+                    LOCATION_PERMISSION_REQUEST_CODE);
+        }
     }
 
     @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        mapView.onDestroy();
+    public void onMapReady(GoogleMap googleMap) {
+        this.googleMap = googleMap;
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        this.googleMap.setMyLocationEnabled(true);
     }
 
     @Override
-    public void onLowMemory() {
-        super.onLowMemory();
-        mapView.onLowMemory();
+    public void onLocationChanged(Location location) {
+        if (isTracking) {
+            if (googleMap != null) {
+                double latitude = location.getLatitude();
+                double longitude = location.getLongitude();
+                LatLng currentLocation = new LatLng(latitude, longitude);
+
+                if (polylineOptions == null) {
+                    polylineOptions = new PolylineOptions();
+                    polylineOptions.width(5);
+                    polylineOptions.color(Color.BLUE);
+                }
+                polylineOptions.add(currentLocation);
+                googleMap.addPolyline(polylineOptions);
+
+                googleMap.moveCamera(CameraUpdateFactory.newLatLng(currentLocation));
+            }
+        }
     }
 
     @Override
@@ -138,39 +147,11 @@ public class MainActivity extends FragmentActivity implements OnMapReadyCallback
 
         if (requestCode == LOCATION_PERMISSION_REQUEST_CODE) {
             if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                // The user granted location permissions, you can now enable location tracking
-                if (googleMap != null) {
-                    if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
-                            && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-                        return;
-                    }
-                    googleMap.setMyLocationEnabled(true);
+                if (isTracking) {
+                    requestLocationUpdates();
                 }
             } else {
                 Toast.makeText(this, "Location permission is needed to display your location on the map.", Toast.LENGTH_SHORT).show();
-            }
-        }
-    }
-
-    private void addCurrentLocationToPolyline() {
-        if (googleMap != null) {
-            LocationManager locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
-            if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-                return;
-            }
-            Location location = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
-
-            if (location != null) {
-                double latitude = location.getLatitude();
-                double longitude = location.getLongitude();
-                LatLng currentLocation = new LatLng(latitude, longitude);
-
-                // Move the camera to the user's current location
-                googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(currentLocation, 15));
-
-                // Add the current location to the polyline
-                polylineOptions.add(currentLocation);
-                Log.d("MyApp", "Adding location to polyline: " + currentLocation.toString());
             }
         }
     }
